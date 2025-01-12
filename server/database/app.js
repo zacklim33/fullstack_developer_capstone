dba4496581674e8923a00a7344763bf94b8d0004
liangzhong Lim <limllz13@gmail.com>
2025-01-12 11:53:34 +0800

    added mongoose api

--- server/database/app.js
+++ server/database/app.js
@@ -13,10 +13,9 @@ const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'))
 
 mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 
-
 const Reviews = require('./review');
-
 const Dealerships = require('./dealership');
+
 
 try {
   Reviews.deleteMany({}).then(()=>{
@@ -36,6 +35,7 @@ app.get('/', async (req, res) => {
     res.send("Welcome to the Mongoose API")
 });
 
+
 // Express route to fetch all reviews
 app.get('/fetchReviews', async (req, res) => {
   try {
@@ -45,6 +45,7 @@ app.get('/fetchReviews', async (req, res) => {
     res.status(500).json({ error: 'Error fetching documents' });
   }
 });
+
 
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
@@ -56,20 +57,40 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
   }
 });
 
-// Express route to fetch all dealerships
-app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+
+//Express route to fetch all dealers
+app.get('/fetchDealers', async(req,res) => {
+  try{
+    const dealership = await Dealerships.find();
+    res.json(dealership);
+    } catch(error){
+      res.status(500).json({error:'Error in getting Dealerships'});
+    }
 });
+
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+  try{
+    const dealership = await Dealerships.find({state: req.params.state});
+    res.json(dealership);
+  } catch(error) {
+    res.status(500).json({'error': 'Error in getting dealership info'})
+  }
 });
+
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+  try{
+    const dealership = await Dealerships.find({id : req.params.id})
+    res.json(dealership)
+  } catch(error) {
+    res.status(500).json({'error': 'Error in getting dealership info'})
+  }
 });
+
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
@@ -97,6 +118,7 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
     res.status(500).json({ error: 'Error inserting review' });
   }
 });
+
 
 // Start the Express server
 app.listen(port, () => {
